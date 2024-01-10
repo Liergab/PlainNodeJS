35c6182601e465e9fd8d5d5fe6d2747341b9a358
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-01-10 09:59:19 +0800

    text for fs-read and fs-write

--- index.js
+++ index.js
@@ -22,10 +22,75 @@ const filePath = "/Users/Asus/Bryan/plainnode/files/sample.txt"
 // // extension
 // console.log(`extension => ${path.extname(filePath)}`)
 
-// import { fileURLToPath } from 'url';
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
+
 // console.log(__dirname)
 // console.log(__filename)
-const basename = "hello.txt"
-console.log(path.join(path.dirname(filePath), basename))
+// const basename = "hello.txt"
+// console.log(path.join(path.dirname(filePath), path.basename(filePath)))
+
+import fs from 'fs'
+// reading from afile
+
+// fs.readFile(filePath,'utf-8',(err,data)=>{
+//     //this is for asynchronous
+//     if(err){
+//       throw new Error('something went wrong')
+//     }
+
+//     console.log(data)
+// })
+
+import { fileURLToPath } from 'url';
+import fsPromise from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+
+// const hey = fs.readFileSync(path.join(__dirname, 'files', 'sample.txt'), 'utf-8');
+
+// console.log(hey)
+// console.log(__dirname)
+
+const readingfile = async() => {
+    try {
+        const data = await fsPromise.readFile(filePath, {encoding: 'utf-8'})
+        console.log('FromfsPromise =>',data)
+    } catch (error) {
+        console.log('error from fsPromise')
+    }
+}
+readingfile()
+
+// writing in file
+const textFile = path.join(__dirname, 'files', 'text.txt')
+const content = "Write, Append, Read"
+// fs.writeFile(textFile,content,(err)=>{
+//     //this is for asynchronous
+//     if(err){
+//       throw new Error('something went wrong')
+//     }
+//     console.log('Successful writing using fs.write! =>')
+
+//     fs.readFile(textFile,'utf-8',(err, data) => {
+//         if(err)   throw new Error('something went wrong')
+
+//         console.log(`Nested callback Inside of Fs.write => ${data}`)
+//     })
+// })
+
+// write with async await with manny operation
+
+const writeAppendRead = async() => {
+    try {
+        await fsPromise.writeFile(textFile, content);
+        await fsPromise.appendFile(textFile, "\nThis is text append");
+        const data = await fsPromise.readFile(textFile, 'utf-8')
+        console.log(data)
+    } catch (error) {
+        console.error('something went wrong')
+    }
+}
+
+writeAppendRead()
+
